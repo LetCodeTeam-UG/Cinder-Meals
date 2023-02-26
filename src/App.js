473d545bc778858094cecf14292875c1275218a9
nii_okantey <nunoogideon19@gmail.com>
2023-02-26 10:11:17 +0000

    updated new files

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { Route, Routes } from "react-router-dom";
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import Menu from './components/Menu';
+import TrackOrder from './components/TrackOrder';
+import Contact from './components/Contact';
+import Login from './components/Login';
+import './App.css';
+
+
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route path='/home' element={<Home />}></Route>
+          <Route path='/menu' element={<Menu />}></Route>
+          <Route path='/trackorder' element={<TrackOrder />}></Route>
+          <Route path='/contact' element={<Contact />}></Route>
+          <Route path='/login' element={<Login />}></Route>
+        </Routes>
+      </div>
+    );
+  }
+}
+
+export default App;
