18aa49811ec0fc78c86e8b08c838ef616d59ac3d
Jonathan Kofi Donkor <jkd6735@gmail.com>
2023-02-26 21:59:14 +0000

    update

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import logo from "../../images/logo.png"
+import './navbar.css'
+
 
 const Navbar = () => {
-  return <div className="navbar">Navbar</div>;
+  return <div className="cinder__navbar">
+    <div className="cinder__navbar-links">
+      <div className="cinder__navbar-links_logo">
+        <img src={logo} alt="logo" />
+      </div>
+      <div className="cinder__navbar-links_container">
+        <p><a href="#menu">Menu</a></p>
+        <p><a href="#trackorder">Track Order</a></p>
+        <p><a href="#contact">Contact</a></p>
+      </div>
+    </div>
+    <div className="cinder__navbar-login">
+        <p>Login</p>
+        <button type="button" >Sign Up</button>
+      </div>
+  </div>;
 };
 
 export default Navbar;
