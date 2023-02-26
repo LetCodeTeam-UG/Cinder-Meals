279ecadf215f7fc0990611452196b478e5951b90
nii_okantey <nunoogideon19@gmail.com>
2023-02-26 10:42:55 +0000

    global variables for styling done

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,40 @@
 import React from "react";
+import logo from "../images/logo.png";
 
 const Login = () => {
-  return <div>Login</div>;
+  const formProps = [
+    "Username",
+    "Password",
+    "Login",
+    "Forgot Password?",
+    "Create New Account",
+  ];
+  const loginBtn = <input id="login" placeholder={formProps[2]} />;
+  return (
+    <div className="login-page">
+      <div className="login-container">
+        {/* brand logo goes here */}
+        <span>
+          <img src={logo} alt="cinder" />
+        </span>
+        <form action="">
+          {/* username */}
+          <div className="props">
+            <label htmlFor="username">{formProps[0]}</label>
+            <input type="text" id="password" />
+          </div>
+          {/* password */}
+          <div className="props">
+            <label htmlFor="password">{formProps[1]}</label>
+            <input type="password" id="password" />
+          </div>
+
+          {/* login-btn */}
+          <div>{loginBtn}</div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
