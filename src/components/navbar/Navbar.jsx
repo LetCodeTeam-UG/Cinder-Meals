e9ef3d992321bb7dd4d71d16263322b2f14f325f
Jonathan Kofi Donkor <jkd6735@gmail.com>
2023-03-01 14:39:58 +0000

    update

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,21 @@
-import React from "react";
+import React, {useState} from "react";
 import logo from "../../images/logo.png";
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
 import { Link } from "react-router-dom";
 import { BsCart4 } from "react-icons/bs";
 import "./navbar.css";
 
+const Menu = () => {
+  <>
+  <p><a href="#" >Home</a></p>
+  <p><a href="#" >Menu</a></p>
+  <p><a href="#" >Track Order</a></p>
+  <p><a href="#" >Contact</a></p>
+  </>
+}
+
 const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
   const navLinks = [
     {
       name: "Home",
@@ -25,40 +36,72 @@ const Navbar = () => {
     },
   ];
   return (
-    <nav className="cinder__navbar">
-      <div className="cinder__logo">
+    <div className="cinder__navbar">
+      <div className="cinder__navbar-logo">
         <Link to={navLinks[0].path}>
           {" "}
           <img src={logo} alt="logo" />
         </Link>
       </div>
-      <ul className="cinder__navbar-links">
-        <li>
+      <div className="cinder__navbar-links">
+        <p>
           <Link className="paths" to={navLinks[0].pathTwo}>
             {navLinks[0].name}
           </Link>
-        </li>
-        <li>
+        </p>
+        <p>
           <Link className="paths" to={navLinks[1].path}>
             {navLinks[1].name}
           </Link>
-        </li>
-        <li>
+        </p>
+        <p>
           <Link className="paths" to={navLinks[2].path}>
             {navLinks[2].name}
           </Link>
-        </li>
-        <li>
+        </p>
+        <p>
           <Link className="paths" to={navLinks[3].path}>
             {navLinks[3].name}
           </Link>
-        </li>
+        </p>
         <span className="cart">
           <BsCart4 color="#ed7014" fontSize="2rem" />
           <span className="quantity">0</span>
         </span>
-      </ul>
-    </nav>
+      </div>
+      <div className="cinder__navbar-menu">
+        {toggleMenu ? <RiCloseLine color="var(--primary-color)"  size={27} onClick={() => setToggleMenu(false) } /> : 
+        <RiMenu3Line color="var(--primary-color)"  size={27} onClick={() => setToggleMenu(true) } /> }
+        {toggleMenu && (
+          <div className="cinder__navbar-menu_container">
+            <div className="cinder__navbar-menu_container-links">
+                <div>
+                <Link className="paths" to={navLinks[0].pathTwo}>
+                  {navLinks[0].name}
+                </Link>
+              </div>
+              <div>
+                <Link className="paths" to={navLinks[1].path}>
+                  {navLinks[1].name}
+                </Link>
+              </div>
+              <div>
+                <Link className="paths" to={navLinks[2].path}>
+                  {navLinks[2].name}
+                </Link>
+              </div>
+              <div>
+                <Link className="paths" to={navLinks[3].path}>
+                  {navLinks[3].name}
+                </Link>
+              </div>
+            </div>
+          </div>
+        )  
+        }
+      </div>
+    </div>
+    
   );
 };
 
