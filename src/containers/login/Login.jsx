ef5d315a64d9377637b2a72d1d6e1fe6e29c497f
nii_okantey <nunoogideon19@gmail.com>
2023-03-22 14:02:36 +0000

    set up links for login and register

--- src/containers/login/Login.jsx
+++ src/containers/login/Login.jsx
@@ -6,30 +6,46 @@ import "./login.css";
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
 
-  const handleSubmit = async (event) => {
+  const handleSubmit = (event) => {
     event.preventDefault();
 
-    const response = await fetch("127.0.0.1:8000/api/auth/login", {
+    // perform form validation here
+    if (!email || !password) {
+      setErrorMessage("Please enter a username and password.");
+      return;
+    }
+
+    // create an object containing the form data
+    const loginData = { email, password };
+
+    // make a POST request to the API endpoint
+    fetch("https://example.com/login", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ email, password }),
-    });
-
-    const data = await response.json();
-    const token = data.token;
-
-    // Store the token somewhere (e.g. local storage)
-    localStorage.setItem("token", token);
+      body: JSON.stringify(loginData),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        // handle the response from the API endpoint
+        if (data.token) {
+          // store the token in local storage or a cookie
+          localStorage.setItem("token", data.token);
+          window.location.href = "/home";
+          // redirect to the home page or set a state to display home page
+        } else {
+          setErrorMessage("Invalid username or password.");
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+        setErrorMessage("An error occurred. Please try again.");
+      });
   };
 
-  const loginBtn = (
-    <Link className="to-login" to="/home">
-      <input className="login" type="submit" value="Login" />
-    </Link>
-  );
   return (
     <div className="login-page">
       <div className="login-container">
@@ -37,8 +53,9 @@ const Login = () => {
         <span className="logo">
           <img src={logo} alt="cinder" />
         </span>
-        <form action="" onSubmit={handleSubmit}>
+        <form onSubmit={handleSubmit}>
           {/* username */}
+          {errorMessage && <p>{errorMessage}</p>}
           <div className="props">
             <label htmlFor="email">Email</label>
             <input
@@ -59,24 +76,21 @@ const Login = () => {
             />
           </div>
 
-          {/* login-btn */}
-          <div className="props">{loginBtn}</div>
-          {/* password reset */}
-          <div className="reset">Forgot Password?</div>
-
-          {/* create new account */}
-          <div className="new-user">
-            New to cinder meals?{" "}
-            <span>
-              <Link className="new" to="/signup">
-                Create New Account
-              </Link>
-            </span>
+          <div className="props">
+            <input className="login" type="submit" value="Login" />
           </div>
         </form>
+        <div className="reset">Forgot Password?</div>
+        <div className="new-user">
+          New to cinder meals?{" "}
+          <span>
+            <Link className="new" to="/signup">
+              Create New Account
+            </Link>
+          </span>
+        </div>
       </div>
     </div>
   );
 };
-
 export default Login;
--- src/containers/signUp/SignUp.jsx
+++ src/containers/signUp/SignUp.jsx
@@ -1,23 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import "../login/login.css";
 import logo from "../../images/logo.png";
 import { Link } from "react-router-dom";
 
 const SignUp = () => {
-  const formProps = [
-    "Username",
-    "Email",
-    "Password",
-    "SignUp",
-    "Forgot Password?",
-    "Create New Account",
-    "Login",
-  ];
-  const loginBtn = (
-    <Link className="to-login" to="/auth/login">
-      <input className="login" type="submit" value={formProps[5]} />
-    </Link>
-  );
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const [error, setError] = useState("");
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormData((prevState) => ({ ...prevState, [name]: value }));
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    // perform form validation here
+    if (formData.password !== formData.confirmPassword) {
+      setError("Passwords do not match");
+      return;
+    }
+
+    // make API call to post the data
+    const response = await fetch("https://your-api-endpoint.com/signup", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    });
+
+    if (response.ok) {
+      // redirect to the login page
+      window.location.href = "/login";
+    } else {
+      const errorData = await response.json();
+      setError(errorData.message);
+    }
+  };
   return (
     <div className="login-page">
       <div className="login-container">
@@ -25,39 +49,53 @@ const SignUp = () => {
         <span className="logo">
           <img src={logo} alt="cinder" />
         </span>
-        <form action="">
-          {/* username */}
+        <form onSubmit={handleSubmit}>
+          {error && <p>{error}</p>}
           <div className="props">
-            <label htmlFor="username">{formProps[0]}</label>
-            <input type="text" id="username" />
-          </div>
-
-          <div className="props">
-            <label htmlFor="email">{formProps[1]}</label>
-            <input type="email" id="email" />
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              name="email"
+              value={formData.email}
+              onChange={handleInputChange}
+              required
+            />
           </div>
 
           {/* password */}
           <div className="props">
-            <label htmlFor="password">{formProps[2]}</label>
-            <input type="password" id="password" />
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              name="password"
+              value={formData.password}
+              onChange={handleInputChange}
+              required
+            />
+          </div>
+          <div className="props">
+            <label htmlFor="confirmPassword">Confirm Password</label>
+            <input
+              type="password"
+              name="confirmPassword"
+              value={formData.confirmPassword}
+              onChange={handleInputChange}
+              required
+            />
           </div>
 
-          {/* login-btn */}
-          <div className="props">{loginBtn}</div>
-          {/* password reset */}
-          {/* <div className="reset">{formProps[3]}</div> */}
-
-          {/* create new account */}
-          <div className="new-user">
-            Already Have An Account?{" "}
-            <span>
-              <Link className="new" to="/auth/login">
-                {formProps[6]}
-              </Link>
-            </span>
+          <div className="props">
+            <input className="login" type="submit" value="Register" />
           </div>
         </form>
+        <div className="new-user">
+          Already Have An Account?
+          <span>
+            <Link className="new" to="/auth/login">
+              Login
+            </Link>
+          </span>
+        </div>
       </div>
     </div>
   );
